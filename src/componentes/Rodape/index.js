2cfc3ec9d5158cdbf99fbd2a0deced313dd3d331
Lenilson Rosa <lenilsonrr86@gmail.com>
2023-09-08 00:35:02 -0300

    incluindo rodape

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,14 @@
+import './rodape.css'
+
+const Rodape = () => {
+    return (
+        <header className='rodape'>
+            <img src='/imagens/rodape.png' alt="imagem do rodape" />
+           
+
+        </header>
+    )
+}
+
+
+export default Rodape
